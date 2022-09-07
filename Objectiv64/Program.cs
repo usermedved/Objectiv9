59db3e74a69893cb875f04c099b11ee047dd9713
Sergei Medvedev <clip1988@yandex.ru>
2022-09-07 21:44:23 +0300

    Objectiv 64 project

---
+++ Objectiv64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine();
+Console.Write("Введите натуральное число N: ");
+int number_N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int number_M = 1;
+
+int NaturalNumber(int num_M, int num_N)
+{
+    if (num_M == num_N)
+        return num_N;
+    else
+        Console.Write($"{NaturalNumber(num_M, num_N - 1)}, ");
+    return num_N;
+}
+
+Console.WriteLine(NaturalNumber(number_M, number_N));
